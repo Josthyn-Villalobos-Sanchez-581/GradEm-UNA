ff5aa6d9834e0a7b2943f7131a89b810e2bbe5a9
Kevin Beita <kevin.beita.marin@est.una.ac.cr>
2025-09-24 22:05:09 -0600

    Update perfil update response and style button in Perfil page

--- backend/resources/js/pages/Perfil/Index.tsx
+++ backend/resources/js/pages/Perfil/Index.tsx
@@ -454,7 +454,7 @@ export default function Index({ usuario, areaLaborales, paises, provincias, cant
             {/* Botones */}
             <button
               type="submit"
-              className="bg-green-600 hover:bg-green-800 text-white px-4 py-2 rounded col-span-2"
+              className="bg-[#034991] hover:bg-[#0563c1] text-white px-4 py-2 rounded col-span-2"
             >
               Guardar cambios
             </button>
